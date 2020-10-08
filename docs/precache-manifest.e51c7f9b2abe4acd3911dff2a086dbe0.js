ea7cfd78c581f4fcb73e26affa9f84a3ca2bc9c2
Paola <pao.alejandra1992@gmail.com>
2020-10-08 11:18:58 -0300

    docs added

---
+++ docs/precache-manifest.e51c7f9b2abe4acd3911dff2a086dbe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e83d6f80a7b6f5f45a4bcee0e3b892e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6213c64e264aae92362b",
+    "url": "/static/css/main.39969db0.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6213c64e264aae92362b",
+    "url": "/static/js/main.773d0d0f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
