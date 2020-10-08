2e37e3d42014773b3ea118ecf04f9e64643a6d7f
Paola <pao.alejandra1992@gmail.com>
2020-10-08 11:13:12 -0300

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+export const getGifs = async ( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=DaMF2PVKXKHQtfs5wlWY8pNqpVnbz8Co`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
